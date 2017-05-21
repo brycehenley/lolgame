87bbafe4a34ee37b78ca9c2a7093f6bf99b8626b
brycehenley <brycehenley97@gmail.com>
2017-05-21 13:42:04 -0700

    rain

--- main.lua
+++ main.lua
@@ -1,3 +1,38 @@
+function love.load()
+particle = love.graphics.newImage("/assets/rain.png")	
+rain_system = love.graphics.newParticleSystem(particle, 1000)
+
+rain_system:setEmissionRate(100)
+rain_system:setSpeed(1,3)
+rain_system:setLinearAcceleration(1,500,1,600)
+rain_system:setSizes(1.5,0.9,0.5)
+rain_system:setPosition(240,-50)
+rain_system:setEmitterLifetime(-1)
+rain_system:setParticleLifetime(2,3.5)
+rain_system:setDirection(.45)
+rain_system:setAreaSpread("normal",200,0)
+rain_system:setColors(10,10,200,200)
+
+rain_system:start()
+rain = true
+
+end
+
+function love.update(dt)
+
+	if rain then
+		rain_system:update(dt)
+	else
+
+	end
+end
+
 function love.draw()
-	love.graphics.print("Hello World!", 200,135)
+
+	if rain then
+		love.graphics.setBackgroundColor(10,10,10)
+		love.graphics.draw(rain_system)
+	else
+
+	end
 end

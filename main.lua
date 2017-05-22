cf6b675fa82b8fe02b00354d4639f23061807ac7
brycehenley <brycehenley97@gmail.com>
2017-05-21 22:17:29 -0700

    partical optimize

--- main.lua
+++ main.lua
@@ -1,17 +1,17 @@
 function love.load()
-particle = love.graphics.newImage("/assets/rain.png")	
+particle = love.graphics.newImage("/assets/rain.png")
 rain_system = love.graphics.newParticleSystem(particle, 1000)
 
-rain_system:setEmissionRate(100)
+rain_system:setEmissionRate(150)
 rain_system:setSpeed(1,3)
 rain_system:setLinearAcceleration(1,500,1,600)
-rain_system:setSizes(1.5,0.9,0.5)
+rain_system:setSizes(0.99,0.7,1.2)
 rain_system:setPosition(240,-50)
 rain_system:setEmitterLifetime(-1)
-rain_system:setParticleLifetime(2,3.5)
+rain_system:setParticleLifetime(1.0,1.33)
 rain_system:setDirection(.45)
 rain_system:setAreaSpread("normal",200,0)
-rain_system:setColors(10,10,200,200)
+rain_system:setColors(188,188,255,135)
 
 rain_system:start()
 rain = true
@@ -30,7 +30,8 @@ end
 function love.draw()
 
 	if rain then
-		love.graphics.setBackgroundColor(10,10,10)
+		
+love.graphics.setBackgroundColor(100, 40, 0)
 		love.graphics.draw(rain_system)
 	else
 
